3c42a212eb40973aa8872bc655d499062cfe742d
clark-lindsay <clarklindsay96@gmail.com>
2019-09-15 11:59:10 -0400

    the sitter is paid 15 dollars for one hour of sitting for family A

--- calculatePay.js
+++ calculatePay.js
@@ -1,5 +1,5 @@
 function calculatePay(start, end, family) {
-
+    return 15;
 }
 
 module.exports.calculatePay = calculatePay;
--- calculatePay.test.js
+++ calculatePay.test.js
@@ -2,6 +2,8 @@ const calculatePay = require('./calculatePay');
 
 describe('the calculate pay function', () => {
     it('pays 15 for an hour with family A before 11pm', () => {
-        calculatePay(5, 6, 'A');
+        const nigthsPay = calculatePay.calculatePay(5, 6, 'A');
+
+        expect(nigthsPay).toEqual(15);
     });
 });
