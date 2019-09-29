5abba7ff8f94108e6adc40f565f8683eb35d4f27
clark-lindsay <clarklindsay96@gmail.com>
2019-09-29 13:37:17 -0400

    the clock can tell you when one time is earlier than another, according to the particular working hours of babysitters

--- BabySitterrClock.test.js
+++ BabySitterrClock.test.js
@@ -15,4 +15,11 @@ describe('BabySittersClock', () => {
         expect(clock.calculateHoursWorked(7, 2)).toEqual(7);
         expect(clock.calculateHoursWorked(5, 4)).toEqual(11);
     });
+
+    test('it can tell you if one time is earlier than another', () => {
+        const clock = new BabySittersClock();
+
+        expect(clock.isEarlierThan(5, 2)).toEqual(true);
+        expect(clock.isEarlierThan(2, 5)).toEqual(false);
+    });
 });
--- BabySittersClock.js
+++ BabySittersClock.js
@@ -1,6 +1,6 @@
 class BabySittersClock {
     constructor() {
-
+        this.times = [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4];
     }
 
     calculateHoursWorked(startTime, endTime) {
@@ -9,6 +9,15 @@ class BabySittersClock {
         }
         else {
             return endTime - startTime;
+        }
+    }
+
+    isEarlierThan(timeOne, timeTwo) {
+        if (this.times.indexOf(timeOne) < this.times.indexOf(timeTwo)) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
